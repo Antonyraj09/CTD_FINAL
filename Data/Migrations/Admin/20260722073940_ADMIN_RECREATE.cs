b36b6b430227c7dc1f1277b462dd6c990b51229b
antony <antonyraj@royaltechsystems.co.in>
2026-07-22 13:13:17 +0530

    Admin_DB_Recreate

---
+++ Data/Migrations/Admin/20260722073940_ADMIN_RECREATE.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CTD_FINAL.Data.Migrations.Admin
+{
+    /// <inheritdoc />
+    public partial class ADMIN_RECREATE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
