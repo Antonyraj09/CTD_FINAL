b7beee6ab6e1b4ae0c01e7b24aeac0853b5c73db
antony <antonyraj@royaltechsystems.co.in>
2026-08-18 14:52:32 +0530

    CTD description changes

---
+++ Data/Migrations/20260818075534_AddTransitRouteCode.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CTD_FINAL.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTransitRouteCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RouteCode",
+                table: "TransitRoutes",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RouteCode",
+                table: "JobIsnes",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RouteCode",
+                table: "TransitRoutes");
+
+            migrationBuilder.DropColumn(
+                name: "RouteCode",
+                table: "JobIsnes");
+        }
+    }
+}
