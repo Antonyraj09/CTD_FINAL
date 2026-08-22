bccef0bd60152cdb574f38830eb4e4037be84f12
antony <antonyraj@royaltechsystems.co.in>
2026-08-22 15:35:29 +0530

    Migration

---
+++ Data/Migrations/20260822080422_AddMisctJobDocumentFields.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CTD_FINAL.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMisctJobDocumentFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BondNo",
+                table: "MisctJobs",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PortOfEntryIndia",
+                table: "MisctJobs",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BondNo",
+                table: "MisctJobs");
+
+            migrationBuilder.DropColumn(
+                name: "PortOfEntryIndia",
+                table: "MisctJobs");
+        }
+    }
+}
